705a925ee795ff0dda8e4562accea8271dd54d97
Elijah-05 <125945721+Elijah-05@users.noreply.github.com>
2023-06-03 12:29:48 +0300

    Addis Tech project added

--- src/Data.js
+++ src/Data.js
@@ -1,104 +1,105 @@
-import randomQuote from './images/project-random-quote.png'
-import groceryList from './images/project-my-grocery-list.png'
-import countdown from './images/project-countdown.png'
-import portfolio from './images/project-personal-portfolio2.png'
-import graphic from './images/project-graphic-designs.png'
-import ahmed from './images/ahmed-profile.jpg'
-import samuel from './images/sami-profile.jpg'
-
+import randomQuote from "./images/project-random-quote.png";
+import groceryList from "./images/project-my-grocery-list.png";
+import countdown from "./images/project-countdown.png";
+import graphic from "./images/project-graphic-designs.png";
+import saasImage from "./images/saas-company-template.png";
+import ahmed from "./images/ahmed-profile.jpg";
+import samuel from "./images/sami-profile.jpg";
 
 export const texts = {
-    about: "I Graduated in Civil Engineering from Jimma institute of Technology. In addition, I have also received Frontend Web Development Training Certification from <a. href=''>freecodecamp</a. am a talented, creative,  and passionate graphic designer who can complete a given design task in a specific time. I am eager to learn new things and update my knowledge. I can perform multiple tasks which are related."
-}
+  about:
+    "I Graduated in Civil Engineering from Jimma institute of Technology. In addition, I have also received Frontend Web Development Training Certification from <a. href=''>freecodecamp</a. am a talented, creative,  and passionate graphic designer who can complete a given design task in a specific time. I am eager to learn new things and update my knowledge. I can perform multiple tasks which are related.",
+};
 export const SkillsData = [
-    [
-        {
-            skill: 'React',
-            value: 83,
-        },
-        {
-            skill: 'JavaScript',
-            value: 70,
-        },
-        {
-            skill: 'Tailwind',
-            value: 85,
-        },
-        {
-            skill: 'Bootstrap',
-            value: 61,
-        },
-    ],
-    [   
-        {
-            skill: 'Photoshop',
-            value: 90,
-        },
-        {
-        skill: 'Figma',
-        value: 60,
-        },
-        {
-            skill: 'Illustrator',
-            value: 75,
-        },
-        {
-            skill: 'Adobe XD',
-            value: 65,
-        },
-    ]
-]
+  [
+    {
+      skill: "React",
+      value: 83,
+    },
+    {
+      skill: "JavaScript",
+      value: 70,
+    },
+    {
+      skill: "Tailwind",
+      value: 85,
+    },
+    {
+      skill: "Bootstrap",
+      value: 61,
+    },
+  ],
+  [
+    {
+      skill: "Photoshop",
+      value: 90,
+    },
+    {
+      skill: "Figma",
+      value: 60,
+    },
+    {
+      skill: "Illustrator",
+      value: 75,
+    },
+    {
+      skill: "Adobe XD",
+      value: 65,
+    },
+  ],
+];
 
 export const projectData = [
-    {
-        name: 'Random Quote',
-        image: randomQuote,
-        desc: 'Generate random quotes from API with random colors',
-        tech: ['React', 'API', 'CSS'],
-        link: 'https://codepen.io/Elijah-05/full/eYLGypb'
-    },
-    {
-        name: 'My Grocery list',
-        image: groceryList,
-        desc: 'Countdown timer project with an alarm sound. JS algorithm',
-        tech: ['React', 'JS', ''],
-        link: 'https://elijah-05.github.io/my-grocery-list-app/'
-    },
-    {
-        name: '25 + 5 Countdown',
-        image: countdown,
-        desc: 'Countdown timer project with an alarm sound. JS algorithm',
-        tech: ['React', 'JS', ''],
-        link: 'https://codepen.io/Elijah-05/full/KKxxrWX'
-    },
-    {
-        name: 'Portfolio Sample',
-        image: portfolio,
-        desc: 'Simple responsive portfolio page with grid layout system',
-        tech: '',
-        link: 'https://codepen.io/Elijah-05/full/dygWMNp'
-    },
-    {
-        name: 'Graphic Designs',
-        image: graphic,
-        desc: 'Graphic Design works for different companies',
-        tech: '',
-        link: 'https://www.behance.net/Elyas_Abebe'
-    },
-]
+  {
+    name: "Random Quote",
+    image: randomQuote,
+    desc: "Generate random quotes from API with random colors",
+    tech: ["React", "API", "CSS"],
+    link: "https://codepen.io/Elijah-05/full/eYLGypb",
+  },
+  {
+    name: "My Grocery list",
+    image: groceryList,
+    desc: "Countdown timer project with an alarm sound. JS algorithm",
+    tech: ["React", "JS", ""],
+    link: "https://elijah-05.github.io/my-grocery-list-app/",
+  },
+  {
+    name: "Saas Website Template",
+    image: saasImage,
+    desc: "Saas company website template challenge from internship company",
+    tech: ["React", "JS", ""],
+    link: "https://addis-tech.netlify.app/",
+  },
+  {
+    name: "25 + 5 Countdown",
+    image: countdown,
+    desc: "Countdown timer project with an alarm sound. JS algorithm",
+    tech: ["React", "JS", ""],
+    link: "https://codepen.io/Elijah-05/full/KKxxrWX",
+  },
+  {
+    name: "Graphic Designs",
+    image: graphic,
+    desc: "Graphic Design works for different companies",
+    tech: "",
+    link: "https://www.behance.net/Elyas_Abebe",
+  },
+];
 
 export const testimonialData = [
-    {
-        name: "Ahmed Mohamed",
-        profession: 'Backend Developer',
-        message: 'Elyas is creative frontend developer. He often communicates well with the Backend developers, and takes fedback as an improvment.',
-        avatar: ahmed
-    },
-    {
-        name: "Samuael Adnew",
-        profession: 'Web Developer',
-        message: 'Well designed, and attractive UI/UX. I like the animation, the google map, structure, especially the Brand color choise.',
-        avatar: samuel
-    },
-
-]
+  {
+    name: "Ahmed Mohamed",
+    profession: "Backend Developer",
+    message:
+      "Elyas is creative frontend developer. He often communicates well with the Backend developers, and takes fedback as an improvment.",
+    avatar: ahmed,
+  },
+  {
+    name: "Samuael Adnew",
+    profession: "Web Developer",
+    message:
+      "Well designed, and attractive UI/UX. I like the animation, the google map, structure, especially the Brand color choise.",
+    avatar: samuel,
+  },
+];
